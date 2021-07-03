b90030810d12cf53c1067d983cbefa76b305b345
Srog1989 <s.rogers0715@yahoo.com>
2021-07-03 10:23:59 -0600

    added favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,15 @@
 require("@rails/ujs").start()
 require("@jquery").start()
 require("turbolinks").start()
-require("semantic-ui")
+require("semantic-ui").start()
 require("@rails/activestorage").start()
+require("require_tree").start()
 require("channels")
 
 
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
