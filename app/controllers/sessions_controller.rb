7bb18f3cde3bebf84293e9d75f114f0a6b48b3f7
Srog1989 <s.rogers0715@yahoo.com>
2021-07-10 20:32:04 -0600

    added create action and route to sessions controller and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,16 @@ class SessionsController < ApplicationController
     def new
         
     end
+
+    def create
+       user = user.find_by(username: params[:session] [:username])
+       if user && user.authenticate(params [:session] [:password])
+        session[:user_id] = user.id
+        # flash[:success] = "Login successful"
+        redirect_to root_path
+       else
+        # flash.now[:error] = "Incorrect username or password"
+        render 'new'
+       end
+    end
 end
