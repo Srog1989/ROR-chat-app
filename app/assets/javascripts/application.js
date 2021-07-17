06c22672ea55a5e39717cd27ddb64001de857653
Srog1989 <s.rogers0715@yahoo.com>
2021-07-17 15:07:46 -0600

    added auto scroll to chat window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,19 @@ require("require_tree").start();
 require("channels");
 // require("@rails/ujs");
 
-// $(document).on('turbolinks:load', function() {
-//     $('.ui.dropdown').dropdown();
-//     $('.message .close').on('click', function() {
-//       $(this).closest('.message').transition('fade');
-//     });
+scroll_bottom = function () {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+};
 
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+  scroll_bottom();
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
