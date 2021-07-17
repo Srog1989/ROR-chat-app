8d710ff59bcfe83e505b1aee667b28367ea46c1c
Srog1989 <s.rogers0715@yahoo.com>
2021-07-17 09:27:41 -0600

    display messages using partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,19 @@ before_action :require_user
 def create
         message = current_user.messages.build(message_params)
         if message.save
-            redirect_to root_path
+            ActionCable.server.broadcast "chatroom_channel", 
+                                            mod_message: message_render(message)  
+                                                           #sends key of mod message from coffee file 
         end
     end
 
     private
     def message_params
         params.require(:message).permit(:body)
+    end
+
+    def message_render(message)
+        render(partial: 'message', locals: {message: message})
     end
 
 end
