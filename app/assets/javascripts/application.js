d101b8fb385043bb3c56b5953fb0044dc308fdb7
Srog1989 <s.rogers0715@yahoo.com>
2021-07-17 20:18:27 -0600

    modified input box and created custome scope method

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,21 @@ scroll_bottom = function () {
   }
 };
 
+submit_message = function () {
+  $("#message_body").on("keydown", function (e) {
+    if (e.keyCode == 13) {
+      $("button").click();
+      e.target.value = "";
+    }
+  });
+};
+
 $(document).on("turbolinks:load", function () {
   $(".ui.dropdown").dropdown();
   $(".message .close").on("click", function () {
     $(this).closest(".message").transition("fade");
   });
+  submit_message();
   scroll_bottom();
 });
 // Uncomment to copy all static images under ../images to the output folder and reference
